9e6cc43293a0c2044fa94bad65c21dbdc50be567
Guillaume Faas <59444272+Tr00d@users.noreply.github.com>
2026-09-24 13:37:49 +0200

    fix(postgrest): resolve nullable Value in bare boolean predicates (#432)

--- packages/Postgrest/Postgrest.Tests/Linq/WhereClauseTests.cs
+++ packages/Postgrest/Postgrest.Tests/Linq/WhereClauseTests.cs
@@ -165,6 +165,34 @@ public class WhereClauseTests
     }
 
     [TestMethod]
+    public void Where_ShouldUseColumnName_GivenBareNullableBooleanValueMember()
+    {
+        this.client.Table<NullableFlag>().Where(x => x.IsActive!.Value)
+            .GenerateUrl().Should().Be($"{BaseUrl}/nullable_flag?is_active=eq.True");
+    }
+
+    [TestMethod]
+    public void Where_ShouldUseColumnName_GivenNegatedNullableBooleanValueMember()
+    {
+        this.client.Table<NullableFlag>().Where(x => !x.IsActive!.Value)
+            .GenerateUrl().Should().Be($"{BaseUrl}/nullable_flag?is_active=not.eq.True");
+    }
+
+    [TestMethod]
+    public void Where_ShouldTranslateIntoNotIsNull_GivenNullableHasValueMember()
+    {
+        this.client.Table<NullableFlag>().Where(x => x.IsActive.HasValue)
+            .GenerateUrl().Should().Be($"{BaseUrl}/nullable_flag?is_active=not.is.null");
+    }
+
+    [TestMethod]
+    public void Where_ShouldTranslateIntoIsNull_GivenNegatedNullableHasValueMember()
+    {
+        this.client.Table<NullableFlag>().Where(x => !x.IsActive.HasValue)
+            .GenerateUrl().Should().Be($"{BaseUrl}/nullable_flag?is_active=is.null");
+    }
+
+    [TestMethod]
     public void Where_ShouldNestBooleanMember_GivenAnAndPredicate()
     {
         this.client.Table<KitchenSink>().Where(x => x.BooleanValue && x.IntValue > 3)
